b7f9c62fd4b260decd47c850b6ce61a0bf8cfcd2
Ray Hernandez <hernan43@gmail.com>
2025-09-10 21:19:18 -0400

    Basic auth working

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,13 @@
+class AuthController < ApplicationController
+  allow_unauthenticated_access only: [ :create ]
+
+  def create
+    @current_user = User.find_by(email: params[:email])
+    if @current_user && @current_user.authenticate(params[:password])
+      encoded_token = encode(@current_user)
+      render json: { token: encoded_token }, status: :ok
+    else
+      render json: { error: "Invalid email or password" }, status: :unauthorized
+    end
+  end
+end
